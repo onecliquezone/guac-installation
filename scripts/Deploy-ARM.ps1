f9b7097175950762bcf2c0a7ea3544e7559345f3
johnpatrick23 <johnpatrickpapares@gmail.com>
2022-03-10 21:31:30 +0800

    Add PS to deploy arm tempalte

---
+++ scripts/Deploy-ARM.ps1
@@ -0,0 +1,68 @@
+param(
+    [Parameter(Mandatory=$true)][string]$AzureSubscriptionName,
+    [Parameter(Mandatory=$true)][string]$ResourceGroupName,
+    [Parameter(Mandatory=$true)][string]$GUAC_LOCATION="",
+    
+    [Parameter(Mandatory=$true)][string]$AadWebClientId,
+    [Parameter(Mandatory=$true)][string]$AadWebClientAppKey="",
+    [Parameter(Mandatory=$true)][string]$AadTenantId,
+
+    [Parameter(Mandatory=$true)][string]$FullDeploymentArmTemplateFile="",
+
+    [Parameter(Mandatory=$true)][string]$GUAC_NAME="",
+    [Parameter(Mandatory=$true)][string]$EMAIL_TAG="",
+
+    [Parameter(Mandatory=$true)][string]$ADDRESS_SPACE_PREFIXES="",
+    [Parameter(Mandatory=$true)][string]$SUBNET_ADDRESS_PREFIXES="",
+
+    [Parameter(Mandatory=$true)][string]$ADMIN_USERNAME="",
+    [Parameter(Mandatory=$true)][string]$ADMIN_SSH_KEY="",
+
+    [Parameter(Mandatory=$true)][string]$GUAC_VM_SIZE="",
+
+)
+
+
+$azSecureApplicationKey = $AadWebClientAppKey | ConvertTo-SecureString -AsPlainText -Force
+$azCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AadWebClientId, $azSecureApplicationKey
+$isLoggedIn = [bool](Connect-AzAccount -Credential $azCredential -TenantId $AadTenantId -ServicePrincipal)
+
+if($isLoggedIn){
+    Select-AzSubscription -Subscription $AzureSubscriptionName
+    Write-Host "Deploying Resource Group."
+    New-AzResourceGroup -Name $ResourceGroupName -Location $GUAC_LOCATION
+    Write-Host "Deploying Template."
+
+    $invocation = (Get-Variable MyInvocation).Value 
+    $currentPath = Split-Path $invocation.MyCommand.Path 
+    $rootPath = (get-item $currentPath).parent.FullName
+    $parameterPath = "$($currentPath)\templates\parameters.json"
+
+    $parmeters = Get-Content -Path $parameterPath | ConvertFrom-Json
+   
+
+    $armParameters = @{
+        'GUAC_NAME'=(&{If($GUAC_NAME) {$GUAC_NAME} Else {$parmeters.parameters.GUAC_NAME.value}})
+        'GUAC_LOCATION'=(&{If($GUAC_LOCATION) {$GUAC_LOCATION} Else {$parmeters.parameters.GUAC_LOCATION.value}})
+
+        'EMAIL_TAG'=(&{If($EMAIL_TAG) {$EMAIL_TAG} Else {$parmeters.parameters.EMAIL_TAG.value}})
+        
+        'ADDRESS_SPACE_PREFIXES'=(&{If($ADDRESS_SPACE_PREFIXES) {$ADDRESS_SPACE_PREFIXES} Else {$parmeters.parameters.ADDRESS_SPACE_PREFIXES.value}})
+        'SUBNET_ADDRESS_PREFIXES'=(&{If($SUBNET_ADDRESS_PREFIXES) {$SUBNET_ADDRESS_PREFIXES} Else {$parmeters.parameters.SUBNET_ADDRESS_PREFIXES.value}})
+        
+        'ADMIN_USERNAME'=(&{If($ADMIN_USERNAME) {$ADMIN_USERNAME} Else {$parmeters.parameters.ADMIN_USERNAME.value}})
+        'ADMIN_SSH_KEY'=(&{If($ADMIN_SSH_KEY) {$ADMIN_SSH_KEY} Else {$parmeters.parameters.ADMIN_SSH_KEY.value}})
+
+        'GUAC_VM_SIZE'=(&{If($GUAC_VM_SIZE) {$GUAC_VM_SIZE} Else {$parmeters.parameters.GUAC_VM_SIZE.value}})
+    }
+
+    New-AzResourceGroupDeployment `
+        -Name "GuacTemplate" `
+        -ResourceGroupName $ResourceGroupName `
+        -TemplateFile $FullDeploymentArmTemplateFile `
+        -TemplateParameterObject $armParameters
+}
+else {
+    Write-Error "Invalid Access."
+}
+
